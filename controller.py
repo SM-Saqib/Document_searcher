08cbbfca179e41101fd07da931f03173a18521fc
Syed Muhammad Saqib <32038393+SM-Saqib@users.noreply.github.com>
2022-08-05 10:16:34 +0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,11 +1,23 @@
 from ocr_module import tesseract
+from NER import spacy_ner
+import os
+import pandas
 
 def test_1():
-  image_path = "/content/drive/MyDrive/Document_searcher/recv-fax-56890405-2.png"
-  text_page= tesseract.get_ocr_single_image(image_path)
-  with open(image_path+".txt","w") as file:
-    file.write(text_page)
+  for root, dirs, files in os.walk("/content/drive/MyDrive/Document_searcher/input_images"):
+    for file in files:
+
+      image_path = os.path.join(root,file)
+      text_page= tesseract.get_ocr_single_image(image_path)
+      with open("/content/select_images_ocr/"+file+".txt","w") as file:
+        file.write(text_page)
+
+      ner = spacy_ner()
+      ner._to_spacy_token(text_page)
+
     
+
+
 
 
 test_1()
